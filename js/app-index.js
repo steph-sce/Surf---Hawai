a413fb98917d987014fa5cdd1d599aa7931a3088
Barbara <barbaradibattista@yahoo.fr>
2016-10-24 14:54:09 +0200

    produit angular

--- js/app-index.js
+++ js/app-index.js
@@ -2,7 +2,7 @@
   var app = angular.module('SurfHawai', ['ngRoute']);
 
   app.controller('MainController', function () {
-  
+  });
 
   app.controller('FormController', ['$scope',function($scope){
     $scope.master = {};
@@ -13,11 +13,13 @@
       $scope.user = angular.copy($scope.master);
     };
     $scope.reset();
-
     
   }]);
 
+  app.controller('ProduitsController', function(){
+    this.products = object;
   });
+
 
   app.directive('header', function () {
     return {
@@ -42,12 +44,46 @@
       })
       .when('/produits', {
         templateUrl: 'partials/produits.html',
-        controller: 'produitController'
+        controller: 'ProduitsController'
       })
       .when('/contact', {
         templateUrl: 'partials/contact.html',
         controller: 'contactController'
       });
   });
+
+var object = [
+
+{
+  image: "img/top-bikini-triangle-noir-blanc-billabong.jpg",
+  titre: "Maillots Femmes",
+  content: "Les mailots qui vous rendent jolie et sportive miam vous serez à croquer là dedans FETCH ++ -ALL."
+},
+{
+  image: "img/25530750-Surfer-plage-mode-de-vie-des-gens-homme-surf-fonctionnant-avec-de-bodyboard-dans-l-eau-sur-la-plage--Banque-d'images.jpg",
+  titre: "Maillot Hommes",
+  content: "Vous allez faire craquer Madame avec votre grosse... planche"
+},
+{
+  image: "img/surfboard-design-1.jpg",
+  titre: "Planche de SURF",
+  content: "SurfBoard ceci est une plache ++++ WAOUH, essayer c'est l'adopter"
+},
+{
+  image: "img/collapsible_surf2-gi.jpg",
+  titre: "Accessoires planche de SURF",
+  content: "Accessoires"
+},
+{
+  image: "img/cours.png",
+  titre: "Cours",
+  content: "Cours de SURF"
+},
+{
+  image: "img/surf-cream__71143.1457479031.jpg",
+  titre: "Produit",
+  content: "Produit d'entretien"
+}
+];
 
 })();
